db3f02030c41437a75513df433d0f99634c05b8e
ajaylohar <loharajay0019@gmail.com>
2025-02-03 17:59:11 +0530

    spring backend

---
+++ em-project backend/src/main/java/org/ajay/emproject/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package org.ajay.emproject.repository;
+
+import org.ajay.emproject.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+    //cutom method
+    //List<EmployeeEntity> findByName(String name);
+    //save, delete, finbyId , findall
+}
